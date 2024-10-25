575e8923932c7e883f9cf49a4e18af6d2e257c1f
IAmInept <54045132+IAmInept@users.noreply.github.com>
2024-10-25 09:49:17 +0100

    General Layout

--- src/components/Qualifications.tsx
+++ src/components/Qualifications.tsx
@@ -22,21 +22,23 @@ const Qualifications = () => {
       <p className="HeaderDiv">Qualifications</p>
 >>>>>>> b4937e36c1acae5b87fc3070e25e02d2d801653b
 
-      <div>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam minima
-          sequi fugiat rerum laudantium ullam necessitatibus commodi nesciunt,
-          obcaecati enim mollitia reprehenderit magni voluptatum tempora ipsum
-          pariatur et iste quo!
-        </p>
-      </div>
-      <div>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-          fugiat exercitationem ullam obcaecati. Ducimus nemo accusantium,
-          maiores voluptatum cum eligendi placeat asperiores inventore, nostrum
-          cupiditate dolorem. Minima unde et perferendis.
-        </p>
+      <div className="QualContainer">
+        <div className="QualCard">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+            minima sequi fugiat rerum laudantium ullam necessitatibus commodi
+            nesciunt, obcaecati enim mollitia reprehenderit magni voluptatum
+            tempora ipsum pariatur et iste quo!
+          </p>
+        </div>
+        <div className="QualCard">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+            fugiat exercitationem ullam obcaecati. Ducimus nemo accusantium,
+            maiores voluptatum cum eligendi placeat asperiores inventore,
+            nostrum cupiditate dolorem. Minima unde et perferendis.
+          </p>
+        </div>
       </div>
     </div>
   );
