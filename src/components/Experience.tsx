199881877cf893c21e298637d381a1db86f16363
Gian-Luca Wallace <54045132+IAmInept@users.noreply.github.com>
2024-10-25 10:10:27 +0100

    Merge/Rebase

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,20 +1,11 @@
 import "./CSS/Main.css";
 
 const Experience = () => {
-<<<<<<< HEAD
-    return (
-        <div className="Experience">
-            <p className="MainHeader"> Experience </p>
-        </div>
-    )
-}
-=======
   return (
     <div className="Experience">
       <p className="HeaderDiv">Experience</p>
     </div>
   );
 };
->>>>>>> b4937e36c1acae5b87fc3070e25e02d2d801653b
 
 export default Experience;
--- src/components/Qualifications.tsx
+++ src/components/Qualifications.tsx
@@ -1,26 +1,9 @@
 import "./CSS/Main.css";
 
 const Qualifications = () => {
-<<<<<<< HEAD
-    return (
-        <div className="Qualifications">
-            <p className="MainHeader">Qualifications | Portfolio</p>
-
-            <div className="Portfolio_Card">
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi cum optio illum odio nemo officia illo dolorum molestias a eius dignissimos, itaque ex nostrum, ad nesciunt ut aut incidunt voluptatem!</p>
-            </div>
-
-            <div className="Portfolio_Card">
-                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores consequuntur neque debitis, voluptatibus quae nobis soluta ipsum nesciunt quam tempore quia, explicabo quo sequi harum eos odit odio dicta eligendi.</p>
-            </div>
-        </div>
-    )
-}
-=======
   return (
     <div className="Qualifications">
       <p className="HeaderDiv">Qualifications</p>
->>>>>>> b4937e36c1acae5b87fc3070e25e02d2d801653b
 
       <div className="QualContainer">
         <div className="QualCard">
